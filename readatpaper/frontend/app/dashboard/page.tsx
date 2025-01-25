2dca4add66fcae7c0948aec2e93eb22fee1bcafa
WTL04 <williamluu04@gmail.com>
2025-01-25 15:43:21 -0800

    added NavBar

---
+++ readatpaper/frontend/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import NavBar from '../../components/NavBar';
+import { Lekton } from "next/font/google";
+
+const lektonFont = Lekton({
+    subsets: ["latin"],
+    weight: "400",
+})
+
+export default function Page() {
+    return (
+        <div>
+            <div className={ lektonFont.className}>
+                <NavBar/>
+                DashBoard
+            </div>
+        </div>
+    );
+  }
