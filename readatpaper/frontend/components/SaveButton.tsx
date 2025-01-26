2974887e5a84c754f5ad2210d42213bbd75d33df
WTL04 <williamluu04@gmail.com>
2025-01-26 03:08:45 -0800

    pls work

--- readatpaper/frontend/components/SaveButton.tsx
+++ readatpaper/frontend/components/SaveButton.tsx
@@ -10,7 +10,7 @@ export default function SaveButton({ input }: Props) {
         href={{
           pathname: "/dashboard",
           query: {
-            summary: { input },
+            summary: input,
           },
         }}
       >
@@ -22,7 +22,7 @@ export default function SaveButton({ input }: Props) {
 
 {
   /* <button className="bg-blue-500 text-white rounded mt-8 mb-8 px-4 py-2">
-        Save Summarysdfsdfsdsdfsdfs
+        Save SummarysdfsdfsdsdfsdfsYIPPIE
       </button>
 */
 }
