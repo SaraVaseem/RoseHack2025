9e3b4e02917d315ad8b56fda259515ade820f637
bachtn <bach.tnguyen@outlook.com>
2025-01-25 17:40:27 -0800

    added button component

--- readatpaper/frontend/app/dashboard/page.tsx
+++ readatpaper/frontend/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+'use client'; // This will mark the page component as a Client Component
+
+import { useState } from 'react';
+import Button from '../../components/Button';
 import NavBar from '../../components/NavBar';
 import { Lekton } from "next/font/google";
 
@@ -7,11 +11,19 @@ const lektonFont = Lekton({
 })
 
 export default function Page() {
+    // State to manage whether the tab is open or not
+    const [isTabOpen, setIsTabOpen] = useState(false);
+
+    // Function to toggle the tab's visibility
+    const handleClick = () => {
+        setIsTabOpen((prevState) => !prevState);
+    };
     return (
-        <div>
+        <div className="bg-gradient-to-t from-gray-100 via-gray-200 to-gray-300 min-h-screen">
             <div className={ lektonFont.className}>
                 <NavBar/>
-
+                {/* Button that toggles the tab */}
+                <Button text="Open Tab" className="mt-4" />
                 DashBoard
             </div>
         </div>
