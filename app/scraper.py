1edeb5acb0cc13e0aa67e4f217bfd24d21eb7f26
WTL04 <114207423+WTL04@users.noreply.github.com>
2025-01-25 21:47:30 -0800

    Delete app directory

--- app/scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def fetch_paper(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.get_text()
-    return content
